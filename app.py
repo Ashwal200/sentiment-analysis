7da141175eabd1963ce83974d03f1b2e40fd2b0e
ZoharSimhon <Zoharsimhon13@gmail.com>
2024-07-29 18:21:27 +0300

    add features calculation to app - WIP

--- app.py
+++ app.py
@@ -7,6 +7,19 @@ from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 import nltk
 import json
+import importlib.util
+import numpy as np
+import pandas as pd
+from scipy.sparse import hstack
+
+# import features
+from models.features import calculate_text_features_gbc_tf as GBC_TF
+# ...
+
+module_name_to_func = {
+    'GBC_TF': GBC_TF,
+}
+
 
 # Custom tokenizer
 stop_words = set(stopwords.words('english'))
@@ -107,11 +120,23 @@ def predict():
     model_key = f"{vectorizer}_{classifier}"
     
     if model_key in model_paths:
-        vectorizer, classifier = joblib.load("models/" + model_paths[model_key])
+        vectorizer, classifier = joblib.load("trained_models/" + model_paths[model_key])
         sentence_vect = vectorizer.transform([sentence])
-        prediction = classifier.predict(sentence_vect)
+        
+        # Load the correct feature calculation function
+        try:
+            calculate_text_features = module_name_to_func[model_paths[model_key][:-4]]
+            additional_features = calculate_text_features(sentence)
+
+            # Combine vectorized sentence with additional features
+            sentence_combined_vect = hstack([sentence_vect, np.array([additional_features])])
+        except:
+            sentence_combined_vect = sentence_vect
+            
+        prediction = classifier.predict(sentence_combined_vect)
         print(prediction)
         return jsonify({'prediction': prediction[0]})
+    
     else:
         return jsonify({'error': 'Invalid vectorizer or classifier'})
 
---
+++ models/features.py
@@ -0,0 +1,22 @@
+from nltk import pos_tag, word_tokenize
+import string
+
+# Function to calculate additional features
+def calculate_text_features_gbc_tf(text):
+    words = text.split()
+    # Punctuation Count
+    punctuation_count = sum(1 for char in text if char in string.punctuation)
+    # Digit Count
+    digit_count = sum(1 for char in text if char.isdigit())
+    # POS Tagging
+    pos_tags = pos_tag(word_tokenize(text))
+    noun_count = sum(1 for word, tag in pos_tags if tag.startswith('NN'))
+    # Exclamation Count
+    exclamation_count = text.count('!')
+    # Question Count
+    question_count = text.count('?')
+    # Title Word Count
+    title_word_count = sum(1 for word in words if word.istitle())
+    
+    return (punctuation_count, digit_count, noun_count,
+            exclamation_count, question_count, title_word_count)
